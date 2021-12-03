25ab5cebaf77494a95e53a3c4b1421c04da91922
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-12-02 22:23:11 -0300

    Find a car method added

--- src/repository/base/baseRepository.js
+++ src/repository/base/baseRepository.js
@@ -1,6 +1,15 @@
+const { readFile } = require("fs/promises");
+
 class BaseRepository {
   constructor({ file }) {
     this.id = file;
+  }
+
+  async find(itemId) {
+    const result = JSON.parse(await readFile(this.file));
+    if (!itemId) return result;
+    const car = result.find(({ id }) => id === itemId);
+    return car;
   }
 }
 module.exports = BaseRepository;
